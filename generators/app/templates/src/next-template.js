0f9c6707517759e87cda4a719b0ce6913ca429cd
afeiship <1290657123@qq.com>
2018-07-09 15:57:32 +0800

    issue: fix context

--- generators/app/templates/src/next-template.js
+++ generators/app/templates/src/next-template.js
@@ -1,8 +1,7 @@
 (function () {
 
-  var global = global || this || self || window;
+  var global = global || this || window || Function('return this')();
   var nx = global.nx || require('next-js-core2');
-  var _ = nx.import(['is']);
 
   var Nx<%= ShortProjectName %> = nx.declare('nx.<%= ShortProjectName %>', {
 
