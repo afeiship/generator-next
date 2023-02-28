e317dcfdfb736cf2576a75fab89f4f9b39acf825
afeiship <1290657123@qq.com>
2023-02-28 21:16:12 +0800

    fix: for esm module

--- generators/app/templates/src/index.class.js
+++ generators/app/templates/src/index.class.js
@@ -1,17 +1,15 @@
-(function () {
-  var global = typeof window !== 'undefined' ? window : this || Function('return this')();
-  var nx = global.nx || require('@jswork/next');
-  var defaults = { context: global };
+var global = typeof window !== 'undefined' ? window : this || Function('return this')();
+var nx = global.nx || require('@jswork/next');
+var defaults = { context: global };
 
-  var Nx<%- ctx.classify(String(project_name).slice(5)) %> = nx.declare('nx.<%- ctx.classify(String(project_name).slice(5)) %>', {
-    methods: {
-      init: function (inOptions) {
-        this.options = nx.mix(null, defaults, inOptions);
-      }
+var Nx<%- ctx.classify(String(project_name).slice(5)) %> = nx.declare('nx.<%- ctx.classify(String(project_name).slice(5)) %>', {
+  methods: {
+    init: function (inOptions) {
+      this.options = nx.mix(null, defaults, inOptions);
     }
-  });
-
-  if (typeof module !== 'undefined' && module.exports) {
-    module.exports = Nx <%- ctx.classify(String(project_name).slice(5)) %>;
   }
-})();
+});
+
+if (typeof module !== 'undefined' && module.exports) {
+  module.exports = Nx <%- ctx.classify(String(project_name).slice(5)) %>;
+}
--- generators/app/templates/src/index.pack.js
+++ generators/app/templates/src/index.pack.js
@@ -1,15 +1,13 @@
-(function () {
-  var global = typeof window !== 'undefined' ? window : this || Function('return this')();
-  var nx = global.nx || require('@jswork/next');
-  var defaults = { context: global };
+var global = typeof window !== 'undefined' ? window : this || Function('return this')();
+var nx = global.nx || require('@jswork/next');
+var defaults = { context: global };
 
-  // var Nx<%- ctx.classify(project_name) %>
-  nx.<%- ctx.camelize(String(project_name).slice(5)) %> = function (inOptions) {
-    var options = nx.mix(null, defaults, inOptions);
-    // package codes...
-  };
+// var Nx<%- ctx.classify(project_name) %>
+nx.<%- ctx.camelize(String(project_name).slice(5)) %> = function (inOptions) {
+  var options = nx.mix(null, defaults, inOptions);
+  // package codes...
+};
 
-  if (typeof module !== 'undefined' && module.exports) {
-    module.exports = nx.<%- ctx.camelize(project_name.slice(5)) %>;
-  }
-})();
+if (typeof module !== 'undefined' && module.exports) {
+  module.exports = nx.<%- ctx.camelize(project_name.slice(5)) %>;
+}
