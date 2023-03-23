466105be8c9b1157d4e503d67c026b0b6ffbb995
afeiship <1290657123@qq.com>
2023-03-23 09:24:35 +0800

    feat: rm build scripts

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,16 +1,17 @@
 "use strict";
-const Generator = require('yeoman-generator');;
+const Generator = require("yeoman-generator");
 const getp = require("@jswork/generator-prompts");
 const yoHelper = require("@jswork/yeoman-generator-helper");
 const prompts = getp(["scope", "registry", "project_name", "description"]);
 const globby = require("globby");
 
 module.exports = class extends Generator {
-
   get exportName() {
     const { project_name, type } = this.props || {};
-    if (type === 'class') return 'Nx' + nx.classify(String(project_name).slice(5));
-    if (type === 'pack') return 'nx.' + nx.camelize(String(project_name).slice(5));
+    if (type === "class")
+      return "Nx" + nx.classify(String(project_name).slice(5));
+    if (type === "pack")
+      return "nx." + nx.camelize(String(project_name).slice(5));
   }
 
   async prompting() {
@@ -18,26 +19,32 @@ module.exports = class extends Generator {
     this.log(`Welcome to the stunning "generator-next" generator!`);
     this.props = await this.prompt([
       {
-        type: 'list',
-        name: 'type',
-        message: 'Your nx type(class or pack)?',
-        choices: ['class', 'pack']
+        type: "list",
+        name: "type",
+        message: "Your nx type(class or pack)?",
+        choices: ["class", "pack"],
       },
-      ...prompts
+      ...prompts,
     ]);
   }
 
   writing() {
     const { type } = this.props;
-    const dtype = type === 'class' ? 'pack' : 'class';
+    const classify = type === "class";
+    const dtype = type === "class" ? "pack" : "class";
 
     this.fs.copyTpl(
       globby.sync(this.templatePath("**"), { dot: true }),
       this.destinationPath(),
-      { ...this.props, ctx: yoHelper.ctx, export_name: this.exportName }
+      {
+        ...this.props,
+        classify,
+        ctx: yoHelper.ctx,
+        export_name: this.exportName,
+      }
     );
 
     this.deleteDestination(`src/index.${dtype}.js`);
-    this.moveDestination(`src/index.${type}.js`, 'src/index.js');
+    this.moveDestination(`src/index.${type}.js`, "src/index.js");
   }
 };
